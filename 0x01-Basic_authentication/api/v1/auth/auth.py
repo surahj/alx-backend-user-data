74f85e429d99634574a99d7c8507deea23aaf7c4
surahj <odebodeopeyemi@gmail.com>
2023-02-07 18:23:46 +0100

    Fix error

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -34,3 +34,23 @@ class Auth:
             return True
 
         return False
+
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Determines if a request has valid authorization headers.
+        Args:
+            request: The request to check.
+        Returns:
+            The authorization header if it exists.
+            None otherwise.
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
