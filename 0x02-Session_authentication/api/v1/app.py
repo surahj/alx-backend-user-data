f9c6e658ca0183672b12d72b7fc238aa15c2ea36
surahj <odebodeopeyemi@gmail.com>
2023-02-09 23:27:20 +0100

    Add to Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,9 +37,11 @@ def before_request():
     if auth:
         excluded_paths = ['/api/v1/status/',
                           '/api/v1/unauthorized/',
-                          '/api/v1/forbidden/']
+                          '/api/v1/forbidden/',
+                          '/api/v1/auth_session/login/']
         if auth.require_auth(request.path, excluded_paths):
-            if not auth.authorization_header(request):
+            if not auth.authorization_header(request) and \
+                    not auth.session_cookie(request):
                 abort(401)
             if not auth.current_user(request):
                 abort(403)
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -34,7 +34,7 @@ def view_one_user(user_id: str = None) -> str:
             return jsonify(request.current_user.to_json())
 
     if user_id is None:
-        abort(404)
+        abort(404
     user = User.get(user_id)
     if user is None:
         abort(404)
