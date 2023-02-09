602c05e15dadcabe5912807eb7178cabf8bb7f15
surahj <odebodeopeyemi@gmail.com>
2023-02-09 17:09:16 +0100

    Fix error

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,7 +28,6 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
-
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """
         Returns the user id for the session id.
