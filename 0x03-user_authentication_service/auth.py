5ede4263251d578bf8d5674325fccdbcb5a73b06
surahj <odebodeopeyemi@gmail.com>
2023-02-15 21:25:55 +0100

    Create register user method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -21,3 +21,38 @@ def _hash_password(password: str) -> bytes:
         bytes: The encrypted password.
     """
     return hashpw(password.encode(), gensalt())
+
+
+class Auth:
+    """
+    Auth class to interact with the authentication database.
+    Attributes:
+        _db: The database object.
+    """
+
+    def __init__(self):
+        """
+        Constructor for the Auth class.
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Method that registers a user in the database.
+        Args:
+            email: The email of the user.
+            password: The password of the user.
+        Raises:
+            ValueError: If the user already exists.
+        Returns:
+            User: The user object.
+        """
+        try:
+            users_found = self._db.find_user_by(email=email)
+            if users_found:
+                raise ValueError("User {} already exists".format(email))
+        except NoResultFound:
+            hashed_password = _hash_password(password).decode('utf-8')
+            print(hashed_password)
+            user = self._db.add_user(email, hashed_password)
+            return user
