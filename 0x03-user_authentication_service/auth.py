cbe144b441265b1ed126eaf5625eecd9651c8606
surahj <odebodeopeyemi@gmail.com>
2023-02-16 18:45:36 +0100

    Fix error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -124,8 +124,6 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            if user.reset_token:
-                return user.reset_token
             token = _generate_uuid()
             self._db.update_user(user.id, reset_token=token)
             return token
