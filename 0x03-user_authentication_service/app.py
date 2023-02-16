951638f7c73c51e1387ca72f028641b4d5a193ff
surahj <odebodeopeyemi@gmail.com>
2023-02-16 18:35:19 +0100

    Create get_reset_password_token

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -76,3 +76,15 @@ def profile() -> tuple:
     if not user:
         abort(403)
     return jsonify({"email": user.email}), 200
+
+
+def get_reset_password_token(self, email: str) -> str:
+        """ Get reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
