539133a8127a8e6da73a8b51c8b8896bc1362b8b
surahj <odebodeopeyemi@gmail.com>
2023-02-16 18:40:05 +0100

    Fix error

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -118,3 +118,16 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except ValueError:
             pass
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Get reset password token
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user.reset_token:
+                return user.reset_token
+            token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+        except NoResultFound:
+            raise ValueError
